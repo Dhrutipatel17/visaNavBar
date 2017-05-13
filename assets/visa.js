ccaa530c07a5c353b2edc1f5122a7fd4682cff4c
Billy Franz <Billy Franz>
2017-05-12 23:34:12 -0700

    menu icons for categories in mobile

--- assets/visa.js
+++ assets/visa.js
@@ -21,4 +21,19 @@ $(document).ready(function() {
         $('.glyphicon-triangle-top').toggleClass('carrot'); // remove carrot
     });
 
+    $('.nav > li').on('click', function(event) {
+    	console.log(event.target) // test to see what click hits (event is a tag)
+    	$(event.target).children().toggleClass('glyphicon-menu-down');
+    	$(event.target).children().toggleClass('glyphicon-menu-right');
+    })
+
 })
+
+// Used Vanilla JS to determine if category did not have sub-categories (menu span means it does)
+var categories = document.getElementsByClassName('category')
+
+for(var i = 0; i < categories.length; i++) {
+	if(categories[i].children.length === 0) {
+		categories[i].className += ' noSubs'
+	}
+}
