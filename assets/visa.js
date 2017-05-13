1bc1d1e20ce43a3fc229999ba34511f8dd79b21a
Billy Franz <Billy Franz>
2017-05-12 18:50:25 -0700

    slide down/up for search field

--- assets/visa.js
+++ assets/visa.js
@@ -1,7 +1,25 @@
 $(document).ready(function() {
 
+	// Dropdown animations
+
+	// Border color
 	$('#search').on('click', function() {
-		$('.navbar').toggleClass('borderColor')
+		$('.navbar').toggleClass('borderColor');
 	})
+
+	// Sliding Down
+	$('.dropdown').on('show.bs.dropdown', function() {
+	  	$(this).find('.dropdown-menu').first().stop(true, true).slideDown(400);
+	});
+
+    // Sliding up 
+    $('.dropdown').on('hide.bs.dropdown', function(e){
+        e.preventDefault();
+        $(this).find('.dropdown-menu').first().stop(true, true).slideUp(400, function(){
+            // Reset default attributes
+            $('.dropdown').removeClass('open');
+            $('.dropdown').find('.dropdown-toggle').attr('aria-expanded','false');
+        });
+    });
 
 })
